74bee8e4e9bff6c372723f568a3dc13eb8923751
Ilana Nagib <ilana.nagib@yahoo.com>
2018-05-24 13:36:38 -0500

    pizza-card

--- src/components/StepOne/StepOne.js
+++ src/components/StepOne/StepOne.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+const mapReduxStateToProps = (reduxState) => (
+    { reduxState }
+);
+
 
 class StepOne extends Component {
+    constructor(props) {
+        super(props);
+    }
 
     render() {
         return (
             <div>
-                <h3>Step One</h3>  
+                <h3> Step 1: Select your Pizza</h3>
+                {this.props.reduxState.pizzaReducer.map(pizza => (
+                    <li key={pizza._id}>{pizza.name}{pizza.description}{pizza.cost}</li>
+                ))}
+                {JSON.stringify(this.props)}
             </div>
         );
     }
 }
+
 
 // connect() allows us to dispatch actions
 export default connect()(StepOne);
