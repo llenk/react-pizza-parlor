e154c35d76d879a2d0fae3a8c1a965d08a2ca887
Ilana Nagib <ilana.nagib@yahoo.com>
2018-05-25 13:08:42 -0500

    basic header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,30 +2,30 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 import { connect } from 'react-redux';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import StepOne from '../StepOne/StepOne';
 import StepTwo from '../StepTwo/StepTwo';
 import StepThree from '../StepThree/StepThree';
 import Admin from '../Admin/Admin';
+import Header from '../Header/Header';
 
 class App extends Component {
   render() {
     return (
+      <div>
+      <Header />
       <Router>
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Prime Pizza</h1>
-        </header>
-        <br/>
+        <div>
         <Route exact path='/' component={StepOne} />
         <Route exact path='/two' component={StepTwo} />
         <Route exact path='/three' component={StepThree} />
         <Route exact path='/admin' component={Admin} />
-      </div>
+        </div>
       </Router>
-    );
-  }
+      </div>
+    )
+  };
 }
 
 export default connect()(App);
